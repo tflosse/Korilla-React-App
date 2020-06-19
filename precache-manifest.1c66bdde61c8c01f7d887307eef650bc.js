bdac7e9fcb3f596a761cf5d4ae60be48ba639d6b
Tamara Vahinerii Flosse <tflosse@tamaras-mbp.home>
2020-06-19 09:56:27 -0700

    Updates

---
+++ precache-manifest.1c66bdde61c8c01f7d887307eef650bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "333a7f71ecbda6cb4228e9033fba8143",
+    "url": "/receipts-react-app/index.html"
+  },
+  {
+    "revision": "9ad3c732c4751a349414",
+    "url": "/receipts-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7d96da47b5fe98a83b64",
+    "url": "/receipts-react-app/static/js/2.94fd5c76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/receipts-react-app/static/js/2.94fd5c76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ad3c732c4751a349414",
+    "url": "/receipts-react-app/static/js/main.f96d0715.chunk.js"
+  },
+  {
+    "revision": "3b1bc2e527e7a3f3f39f",
+    "url": "/receipts-react-app/static/js/runtime-main.63bcfba0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/receipts-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
